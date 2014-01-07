252e1deae5c4d82abbe68464cbf2f7ade934dca4
Dmitry Sukhikh <china@pozitive.biz>
2014-01-07 21:53:54 +0700

    Make main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Pozitive::Application.routes.draw do
 
   resources :pages
-  root 'pages#index'
+  root 'pages#welcome'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
