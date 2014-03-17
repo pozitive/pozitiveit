ce8eb38168f4c059f2ce775e40a5d30ba93f1c6a
Dmitry Sukhikh <china@pozitive.biz>
2014-03-17 22:11:30 +0700

    Fixed pages show view

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -20,5 +20,18 @@ module PagesHelper
     HTML
   end
 
+  def manage_page
+    if user_signed_in?
+      raw <<-HTML
+          <nav class="button-bar">
+            <ul class="button-group radius">
+              <li>#{link_to 'Edit', edit_page_path(@page), class: "button"}</li>
+              <li>#{link_to 'Back', pages_path, class: "button"}</li>
+              <li>#{link_to 'Destroy', @page, :method => :delete, class: "button", data: { :confirm => 'Are you sure?' }}</li>
+            </ul>
+          <nav>
+      HTML
+    end
+  end
 
 end
