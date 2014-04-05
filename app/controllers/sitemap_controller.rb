4961c53c53e4ccf09cbe2b6ba9cd15b979f4a199
Dmitry Sukhikh <china@pozitive.biz>
2014-04-05 13:00:02 +0700

    Created sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,9 +3,15 @@ class SitemapController < ApplicationController
 
   def index
     headers['Content-Type'] = 'application/xml'
-    @blogs = Blog.with_translations('en')
+    @locales =I18n.available_locales.each
+    @blogs = Hash.new
+    @static_pages = Hash.new
+    @locales.each do |locale|
+      @blogs[locale] = Blog.with_translations(locale)
+      @static_pages[locale] = ['portfolio', 'pages/our-proccess', 'about_us', 'contact_us', 'message/new']
+    end
     respond_to do |format|
-      format.xml { @pages = Page.all } # sitemap is a named scope
+      format.xml 
     end
   end
 end
