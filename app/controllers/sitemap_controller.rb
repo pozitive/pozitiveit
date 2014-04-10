779b72a9db2532fe19d20693d61edb0f2d5a9754
Dmitry Sukhikh <china@pozitive.biz>
2014-04-10 18:52:20 +0700

    Changed sitemap controller and view

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,13 +3,9 @@ class SitemapController < ApplicationController
 
   def index
     headers['Content-Type'] = 'application/xml'
-    @locales =I18n.available_locales.each
-    @blogs = Hash.new
-    @static_pages = Hash.new
-    @locales.each do |locale|
-      @blogs[locale] = Blog.with_translations(locale)
-      @static_pages[locale] = ['portfolio', 'pages/our-proccess', 'about_us', 'contact_us', 'message/new']
-    end
+    locale =I18n.locale
+    @blogs = Blog.with_translations(locale)
+    @static_pages = ['portfolio', 'pages/our-proccess', 'about_us', 'contact_us', 'message/new']
     respond_to do |format|
       format.xml 
     end
