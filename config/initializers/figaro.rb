30ac53790d8acaa63218742f5b7ff33c5183941f
Dmitry Sukhikh <china@pozitive.biz>
2014-04-22 22:12:38 +0700

    Fixed bug in required configuration keys lazily

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,2 +1,2 @@
-Figaro.require("aws_access_key_id", "aws_secret_access_key", "aws_s3_bucket")
+Figaro.require("carrierwave_aws_access_key_id", "carrierwave_aws_secret_access_key", "carrierwave_aws_s3_bucket")
 
