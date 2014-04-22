2cdf35e89cf8a61335b8d4d3a4c6dd931ff8912d
Dmitry Sukhikh <china@pozitive.biz>
2014-04-22 22:00:52 +0700

    Fixed bug in required configuration keys lazily

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: Figaro.env.aws_access_key_id
+    aws_access_key_id: Figaro.env.aws_access_key_id,
     aws_secret_access_key: Figaro.env.aws_secret_access_key
   }
   config.fog_directory = Figaro.env.aws_s3_bucket
