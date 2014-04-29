448a769a1782867d9fe38d4c745d99d93b707668
Dmitry Sukhikh <china@pozitive.biz>
2014-04-29 23:45:55 +0700

    Fixed sitemap and Amazon S3 access

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -4,7 +4,7 @@ class SitemapController < ApplicationController
   def index
     headers['Content-Type'] = 'application/xml'
     locale =I18n.locale
-    @blogs = Blog.with_translations(locale)
+    @blogs = Blog.all
     @static_pages = ['portfolio', 'pages/our-proccess', 'about_us', 'contact_us', 'message/new']
     respond_to do |format|
       format.xml 
